7830508923547d135f362eec31b8e518de1d91ec
Jonathan  Foster <jfoster91@192.168.1.108>
2018-07-27 21:46:45 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/jonnyb/smack/Services/AuthService.kt
@@ -0,0 +1,4 @@
+package com.example.jonnyb.smack.Services
+
+object AuthService {
+}
---
+++ app/src/main/java/com/example/jonnyb/smack/Utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.jonnyb.smack.Utilities
+
+const val BASE_URL = "https://chattychat91.herokuapp.com/v1/"
+const  val URL_Register = "${BASE_URL}account/register"
